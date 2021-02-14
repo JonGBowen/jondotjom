632ae103138452d28421294561ddd9598299b398
jongbowen <jbowen23@uwo.ca>
2021-02-14 00:38:25 -0500

    change subtitle

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="main text-center flex flex-col items-center justify-center">
     <!-- <logo /> -->
     <h1 class="title">The Organicist</h1>
-    <h2 class="subtitle">A cool blog of Jon Bowen</h2>
+    <h2 class="subtitle">A softly assembled blog</h2>
   </div>
 </template>
 
